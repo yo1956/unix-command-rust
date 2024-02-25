ee2db306b6d9e21b338ed7f70a6578e29a2dfd0d
yo1956 <hm.hr.yossy@gmail.com>
2024-02-25 23:54:33 +0900

    Add catr

---
+++ catr/src/lib.rs
@@ -0,0 +1,85 @@
+use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(reader) => {
+                let mut line_number = 1;
+                for line in reader.lines() {
+                    let line = line?;
+                    if config.number_lines {
+                        println!("{:6}\t{}", line_number, line);
+                        line_number += 1;
+                    } else if config.number_nonblank_lines {
+                        if line.trim().is_empty() {
+                            println!("{}", line);
+                        } else {
+                            println!("{:6}\t{}", line_number, line);
+                            line_number += 1;
+                        }
+                    } else {
+                        println!("{}", line);
+                        line_number += 1;
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("yo1956")
+        .about("Rust cat")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number_lines")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number_lines"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
+    })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
