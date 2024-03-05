7522a38f563a746f99d403bbde345332e8b9bbd3
yo1956 <hm.hr.yossy@gmail.com>
2024-03-06 01:05:20 +0900

    [wip] Implement headr

---
+++ headr/src/lib.rs
@@ -0,0 +1,83 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("yo1956")
+        .about("Rust head")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("The files to print")
+                .required(true)
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("The number of lines to print")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("The number of bytes to print")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: matches
+            .value_of("lines")
+            .map(|s| parse_positive_int(s).unwrap_or_else(|_| panic!("Invalid input for lines")))
+            .unwrap_or(10), // Provide a default value if none is specified
+        bytes: matches
+            .value_of("bytes")
+            .map(|s| parse_positive_int(s).unwrap_or_else(|_| panic!("Invalid input for bytes"))), // Remove the .ok() as it's already an Option
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse::<usize>() {
+        Ok(num) if num > 0 => Ok(num),
+        _ => Err(From::from(val)),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3は正の整数なのでOK
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // 数字でない文字列の場合はエラー
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // 0の場合もエラー
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
